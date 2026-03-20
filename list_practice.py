0c67d6c1ba49a7898e01dd3e32ba486f8f00c157
PGCP-BDA <PGCP-BDA@students.acts.in>
2026-03-20 12:33:58 +0530

    handling lists

--- list_practice.py
+++ list_practice.py
@@ -56,3 +56,116 @@ lst.append([10,20,30])
 
 lst.sort(reverse=True)
 print(lst)
+
+a=1,2,'test',3
+x=(10,20)
+y=(10,)
+print(type(y))
+
+def f1(a,b):
+    a=a+10
+    b=b+20
+    return a,b
+x,y=f1(10,20)
+print(x,y)
+
+print(x,y,sep="$")
+
+#variable no of parameters
+def add(a,b,*t):
+    s=a+b
+    print('tuple',t)
+    for num in t:
+        s=s+num
+    return s
+
+print(add(12,34))
+print(add(1,2,3,5,23,24))
+
+a=1,2,3,4
+print(a.index(3))
+
+lst=list(a)
+print(lst)
+
+a=tuple(lst)
+print(a)
+
+lst1=[]
+lst=[1,12,3,14,10,7,3,4]
+for num in lst:
+    if num%2==0 and num>5:
+        lst1.append(num)
+print(lst,lst1)
+
+
+#list compression operator
+lst2=[num for num in lst if num%2==0 and num>5]
+print(lst,lst2)
+
+#filter
+lst3=list(filter(lambda num:num%2==0 and num>5,lst))
+print(lst,lst3)
+
+lst4=[]
+for num in lst:
+    lst4.append(num*num)
+print(lst4)
+
+lst6=[num*num for num in lst]
+print(lst6)
+
+lst5=list(map(lambda num:num*num, lst))
+print(lst5)
+
+import functools
+s=0
+for num in lst:
+    s=s+num
+print(s)
+
+s=functools.reduce(lambda acc,num:acc+num,lst)
+print(s)
+
+s=functools.reduce(lambda acc, num:acc+num, lst, 100)
+print(s)
+
+lst=['python', 'perl', 'java']
+s=functools.reduce(lambda acc,s:acc+s, lst)
+print(s)
+
+import functools
+s=fucntools.reduce(lambda acc,s:acc+s[0:3],lst,'')
+print(s)
+
+
+lst=[34, 20, 1, 4, 56, 37]
+for num in sorted(lst):
+    print(num)
+print(lst)
+
+for num in sorted(lst,reverse=True):
+    print(num)
+print(lst)
+
+for num in reversed(lst):
+    print(num)
+print(lst)
+
+lst1=[1,2,3,4]
+lst2=['Delhi','Pune','Mumbai']
+for x,y in zip(lst1,lst2):
+    print(x,'----->',y)
+
+for x in zip(lst1,lst2):
+    print(x[0],'----->',x[1])
+
+lst=[12,23,11,4,3]    
+for i,num in enumerate(lst):
+    print(i,'---->',num)
+
+for num in enumerate(lst):
+    print(num)
+    print(num[0],'---->',num[1])
+    
+    
